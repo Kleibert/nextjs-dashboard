915ce7a2c0ec37089e45b436d986fc48dda11bdd
kaquino <kleibert.aquino@gmail.com>
2025-02-16 16:38:44 -0500

    push project on git

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Customer',
+};
+
+export default function CustomerPage(){
+    return <div>Customer page</div>;
+}   
